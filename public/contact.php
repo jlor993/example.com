235fbb10aa3389233df69afb10ad20eafc653402
Jeffrey Lor <jlor993@gmail.com>
2021-09-08 15:13:09 -0500

    Complete Post CRUD

--- public/contact.php
+++ public/contact.php
@@ -1,130 +1,48 @@
 <?php
-//Include non-vendor files
-require '../core/About/src/Validation/Validate.php';
 
-//Declare Namespaces
-use About\Validation;
+require '../core/processContactForm.php';
 
-//Validate Declarations
-$valid = new About\Validation\Validate();
-$args = [
-  'first_name'=>FILTER_SANITIZE_STRING,
-  'last_name'=>FILTER_SANITIZE_STRING,
-  'subject'=>FILTER_SANITIZE_STRING,
-  'message'=>FILTER_SANITIZE_STRING,
-  'email'=>FILTER_SANITIZE_EMAIL,
-];
-$input = filter_input_array(INPUT_POST, $args);
+$meta = [];
+$meta['description'] = "Contact For Jeffrey Lor";
+$meta['keywords'] = "Contact, Web Development, Jeffrey Lor";
 
-if(!empty($input)){
+$content = <<<EOT
+<form action="contact.php" method="POST">
+    <input type="hidden" name="subject" value="New submission!">
+  
+  <div class="form-control">
+  <label for="name">Name</label>
+    <input id="name" type="text" name="name" value="{$valid->userInput('name')}">
+    <div class="text-error">{$valid->error('name')}</div>
+  </div>
+  <div class="form-control">
+  <label for="email">Email</label>
+    <input id="email" type="text" name="email" value="{$valid->userInput('email')}">
+    <div class="text-error">{$valid->error('email')}</div>
+  </div>
+  <div class="form-control">
+  <label for="message">Message</label>
+    <textarea id="message" name="message">{$valid->userInput('message')}</textarea>
+    <div class="text-error">{$valid->error('message')}</div>
+  </div>
+  <div class="form-control">
+  <input type="submit" value="Send">
+  </div>
+</form>
+<script>
+  var toggleMenu = document.getElementById('toggleMenu');
+  var nav = document.querySelector('nav');
+  toggleMenu.addEventListener(
+    'click',
+    function(){
+      if(nav.style.display=='block'){
+        nav.style.display='none';
+      }else{
+        nav.style.display='block';
+      }
+    }
+  );
+</script>
+EOT;
 
-    $valid->validation = [
-        'first_name'=>[[
-            'rule'=>'notEmpty',
-            'message'=>'Please enter your first name'
-        ]],
-        'last_name'=>[[
-            'rule'=>'notEmpty',
-            'message'=>'Please enter your last name'
-        ]],
-        'email'=>[[
-                'rule'=>'email',
-                'message'=>'Please enter a valid email'
-            ],[
-                'rule'=>'notEmpty',
-                'message'=>'Please enter an email'
-        ]],
-        'subject'=>[[
-            'rule'=>'notEmpty',
-            'message'=>'Please enter a subject'
-        ]],
-        'message'=>[[
-            'rule'=>'notEmpty',
-            'message'=>'Please add a message'
-        ]],
-    ];
-
-
-    $valid->check($input);
-}
-
-if(empty($valid->errors) && !empty($input)){
-    $message = "<div>Success!</div>";
-}else{
-    $message = "<div>Error!</div>";
-}
-
-?>
-
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <title>Jeffrey Lor Contact</title>
-        <meta name="description" content="Jeffrey Lor Contact">
-        <meta name="keywords" content="Jeffrey, Lor, contact, mobile, web, developer">
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-        <link href="./dist/css/main.min.css" type="text/css" rel="stylesheet">
-    </head>
-    <body>
-      <header>
-        <nav class="navbar sticky-top navbar-dark bg-dark">
-          <div class="container-fluid">
-            <a class="navbar-brand" href="index.html">My Website</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-              <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-              <div class="navbar-nav">
-                <a class="nav-link" href="index.html">Home</a>
-                <a class="nav-link" href="resume.html">Resume</a>
-                <a class="nav-link active" aria-current="page" href="contact.html">Contact</a>
-              </div>
-            </div>
-          </div>
-        </nav>
-      </header>
-      <h1>Contact Jeffrey Lor</h1>
-      <p class="left-indent">As an up and coming Mobile and Web Developer, I'm always looking for new opportunities. Please don't hesitate to reach out!</p>
-      <?php echo (!empty($message)?$message:null); ?>
-
-      <form action="contact.php" method="POST">
-        
-        <input type="hidden" name="subject" value="New submission!">
-        
-        <div>
-          <label for="first_name">First Name</label>
-          <input id="first_name" type="text" name="first_name" value="<?php echo $valid->userInput('first_name'); ?>">
-          <div class="text-error"><?php echo $valid->error('first_name'); ?></div>
-        </div>
-
-        <div>
-          <label for="last_name">Last Name</label>
-          <input id="last_name" type="text" name="last_name" value="<?php echo $valid->userInput('last_name'); ?>">
-          <div class="text-error"><?php echo $valid->error('last_name'); ?></div>
-        </div>
-
-        <div>
-          <label for="email">Email</label>
-          <input id="email" type="text" name="email" value="<?php echo $valid->userInput('email'); ?>">
-          <div class="text-error"><?php echo $valid->error('email'); ?></div>
-        </div>
-
-        <div>
-          <label for="message">Message</label>
-          <textarea id="message" name="message"><?php echo $valid->userInput('message'); ?></textarea>
-          <div class="text-error"><?php echo $valid->error('message'); ?></div>
-        </div>
-
-        <div>
-          <input type="submit" value="Send">
-        </div>
-
-      </form>
-    </body>
-</html>
+include '../core/layout.php';
---
+++ public/posts/view.php
@@ -0,0 +1,17 @@
+<?php
+include '../../core/db_connect.php';
+
+$input = filter_input_array(INPUT_GET);
+$slug = preg_replace("/[^a-z0-9-]+/", "", $input['slug']);
+
+$content=null;
+$stmt = $pdo->prepare('SELECT * FROM posts WHERE slug = ?');
+$stmt->execute([$slug]);
+
+$row = $stmt->fetch();
+$content .= "<h1>{$row['title']}</h1>";
+
+$content .="<br><a href=\"edit.php?id={$row['id']}\">Edit Post</a><br>";
+$content .="<br><a href=\"delete.php?id={$row['id']}\">Delete Post</a><br>";
+
+echo $content;
